549511c373e79826ad2e96fae0bfb02873fa13ea
Shidul Islam <sishahin093@gmail.com>
2022-02-25 12:25:34 +0600

    35-4 Null Vs Undefined, different ways you will get undefined

---
+++ unfined-null.js
@@ -0,0 +1,36 @@
+
+
+
+let first;
+// console.log(first);
+function second(x, y) {
+    const sum = x + y;
+}
+const result = second(3, 3);
+// console.log(result);
+
+function add(a, b) {
+    const sum = a + b;
+    return;
+    const fun = a + b;
+    return fun;
+    return sum;
+}
+// console.log(add(3, 3));
+function double(a, b) {
+    const result = a * 2;
+    // console.log(b);
+
+}
+double(32);
+
+const fifth = { name: 'shahin', age: 13, location: 'bandarbon' };
+// console.log(fifth.phone);
+
+const sixth = [34, 5, 36, 6, 562, 2, 7];
+// console.log(sixth[7]);
+delete sixth[2];
+// console.log(sixth);
+
+const seventh = undefined;
+console.log(seventh);
