cbfd1744bdff8b1508ebe43c663d189ccca404ee
Shidul Islam <sishahin093@gmail.com>
2022-02-25 16:29:09 +0600

    35-6 Block scope, global scope, Hoisting

---
+++ scope.js
@@ -0,0 +1,24 @@
+
+const favNum = 27; //global variable.
+function add(first, second) { //
+    const result = first + second + favNum;
+    console.log(mood);
+    if (result > 9) {
+        var mood = 'happy';
+        // console.log(mood);
+    }
+    console.log(mood);
+    return result;
+}
+console.log(add(3, 3));
+
+
+
+
+
+
+
+
+
+
+
