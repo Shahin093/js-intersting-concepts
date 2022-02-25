a5bd92a289a8c449dff9d7099387841446b1fbcf
Shidul Islam <sishahin093@gmail.com>
2022-02-25 10:57:37 +0600

    35-2 Primitive data type and non-primitive data type

---
+++ premitive.js
@@ -0,0 +1,14 @@
+
+
+let a = 'hello';
+let b = a;
+
+console.log(a, b);
+a = 'gelo';
+console.log(a, b);
+
+const x = { job: 'web developer' };
+const y = x;
+console.log(x, y);
+x.job = 'front end developer';
+console.log(x, y);
